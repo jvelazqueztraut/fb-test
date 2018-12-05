55c5a3d1535988ec67bf29c49a442b86ab0d5e4b
Javier Velazquez Traut <jvt@fb.com>
2018-12-05 14:41:03 +0000

    Generic add to cart button parsing

--- js/fb-tracking.js
+++ js/fb-tracking.js
@@ -2,48 +2,23 @@
 (function ($) {
     "use strict";
 
-    $( '#addToCart_BAG01' ).click(function() {
-			fbq('track', 'AddToCart', {
-				value: 75,
-				currency: 'USD',
-				content_ids: 'BAG-01',
-				content_type: 'product',
-			});
-		});
-
-		$( '#addToCart_JACK01' ).click(function() {
-			fbq('track', 'AddToCart', {
-				value: 92.50,
-				currency: 'USD',
-				content_ids: 'JACK-01',
-				content_type: 'product',
-			});
-		});
-
-		$( '#addToCart_COACH01' ).click(function() {
-			fbq('track', 'AddToCart', {
-				value: 165.90,
-				currency: 'USD',
-				content_ids: 'COACH-01',
-				content_type: 'product',
-			});
-		});
-
-		$( '#addToCart_SHORTS01' ).click(function() {
-			fbq('track', 'AddToCart', {
-				value: 15.90,
-				currency: 'USD',
-				content_ids: 'SHORTS-01',
-				content_type: 'product',
-			});
-		});
-
-    $( '#addToCart_MUG01' ).click(function() {
-			fbq('track', 'AddToCart', {
-				value: 22,
-				currency: 'USD',
-				content_ids: 'MUG-01',
-				content_type: 'product',
+    $('.block2-btn-addcart').each(function(){
+			var productName = $(this).parent().parent().parent().find('.block2-name').text();
+			var productId = $(this).parent().parent().parent().find('.block2-id').text().split(' ').pop();
+			if( $(this).parent().parent().parent().find('.block2-newprice').html() ){
+				var productPrice = Number($(this).parent().parent().parent().find('.block2-newprice').text().replace(/[^0-9.-]+/g,""));
+			}
+			else{
+				var productPrice = Number($(this).parent().parent().parent().find('.block2-price').text().replace(/[^0-9.-]+/g,""));
+			}
+			$(this).on('click', function(){
+				fbq('track', 'AddToCart', {
+					value: productPrice,
+					currency: 'USD',
+					content_ids: productId,
+					content_type: 'product',
+				});
+				//swal(productName, "is added to cart !", "success");
 			});
 		});
 
